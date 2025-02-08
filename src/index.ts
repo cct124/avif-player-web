5100bf7d0a06b4be3165aee0a95e5d92e4eeb9a6
Jane <huangdonghao1997@gmail.com>
2025-02-08 11:21:53 +0800

    更改模块导出方式

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import AvifPlayer from "./AvifPlayer";
-import { AvifPlayerWebChannel } from "./types/index";
-
-export default {
-  AvifPlayer,
-  AvifPlayerWebChannel,
-};
+export * from "./AvifPlayer";
+export * from "./types/index";
