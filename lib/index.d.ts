9002e5883f2948a1da37d128b3ed6a6795d100dd
Jane <huangdonghao1997@gmail.com>
2025-02-08 11:23:07 +0800

    构建

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,2 @@
-import AvifPlayer from "./AvifPlayer";
-import { AvifPlayerWebChannel } from "./types/index";
-declare const _default: {
-    AvifPlayer: typeof AvifPlayer;
-    AvifPlayerWebChannel: typeof AvifPlayerWebChannel;
-};
-export default _default;
+export * from "./AvifPlayer";
+export * from "./types/index";
